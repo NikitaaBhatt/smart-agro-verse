e80c98873470eb8dd19efff3d306d46c341b3c79
NikitaaBhatt <nikitabhatt1218@gmail.com>
2026-04-07 15:59:59 +0530

    fixed backend API integration

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 //const BASE_URL = "https://nikita118.pythonanywhere.com/api";
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://nikita118.pythonanywhere.com/api";
 
 // ---------------------
 // Types
